2c937f620e3eb9f4c7fa1c080b9b1c0e2dc31005
OTSGE <199041525+otsge@users.noreply.github.com>
2026-05-14 09:50:30 +0000

    forgejo: add 15.0.1 bottle.

--- Formula/forgejo.rb
+++ Formula/forgejo.rb
@@ -6,6 +6,16 @@ class Forgejo < Formula
   license "GPL-3.0-or-later"
   head "https://codeberg.org/forgejo/forgejo.git", branch: "forgejo"
 
+  bottle do
+    root_url "https://github.com/otsge/homebrew-brews2/releases/download/forgejo-15.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "40229f04c7a23f2927132701d39765fc9d45191ccc65a1e0eca1c0b39268a85b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "919d8c7b5340678af1e4bab623abf0c4926d6da2989f9dec9373b140b0644869"
+    sha256 cellar: :any_skip_relocation, tahoe:         "dcb3441aace5425884ea2d5b4bfe1f6c1ce79d944582e5644c65f09092bb6e84"
+    sha256 cellar: :any_skip_relocation, sequoia:       "38e6c343687e53ef156b7dfca92f8a20bae65b8548e088cc9f2a78ca6d3b1d2c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fa30a892fa435c2a68f3a6f41f33ab45952af0b63f1a208cded57faaa1c168ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "606223c55bf6f0153045216dc02cb7a79c49da8e063c4dee2f14dc104961f66d"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 
