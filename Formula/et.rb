b3fa394293a5d794a2a4b2e31c3c9a56e96b673a
OTSGE <199041525+otsge@users.noreply.github.com>
2026-05-26 16:05:22 +0000

    et: add 6.2.11 bottle.

--- Formula/et.rb
+++ Formula/et.rb
@@ -6,6 +6,16 @@ class Et < Formula
   license "Apache-2.0"
   head "https://github.com/MisterTea/EternalTerminal.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/otsge/homebrew-brews2/releases/download/et-6.2.11"
+    sha256 cellar: :any,                 arm64_tahoe:   "7d721c44fdc74dd2359a80fb0ab612f18f54f02f8f4345a745a3b2f2893e8e6e"
+    sha256 cellar: :any,                 arm64_sequoia: "b2a26751a35d228baa5a4fe092dce36a2edeb119d615c396abdadbbb0769829c"
+    sha256 cellar: :any,                 tahoe:         "526f1c7bd88c018d693eb91b618364855f63e982ff8335755f8d04e0b57e3776"
+    sha256 cellar: :any,                 sequoia:       "35bcda0f2340e54e33d42f187a37670ae0d5edde5992a329b56223a60ad8537d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c0e8bb9bcc2e1173b959287408d44ad17fbfd861609a8b31f078d938a84670e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e628e68684a7498462a7eace2baa59da693fd39cb896d66e08551a9fb05c7ff0"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
 
