bc1c027fbe67b0a27b30bbea064269c57cdccb43
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-09 17:30:31 +0000

    zerobrew 0.3.1
    Closes #14.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Formula/z/zerobrew.rb
+++ Formula/z/zerobrew.rb
@@ -1,8 +1,8 @@
 class Zerobrew < Formula
   desc "Fast package manager alternative to Homebrew, written in Rust"
   homepage "https://github.com/lucasgelfond/zerobrew"
-  url "https://github.com/lucasgelfond/zerobrew/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "47e325a8de0b104fd9ee4a12062ba60b7edd225c951b3bea047603750dd760f1"
+  url "https://github.com/lucasgelfond/zerobrew/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "e35b4f20a04866e67c553e2467f9f57e254b67ada1a2e53c74aa9fbf174f5a3d"
   license all_of: ["Apache-2.0", "MIT"]
   head "https://github.com/lucasgelfond/zerobrew.git", branch: "main"
 
