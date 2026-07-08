ada6398712935e19bff3603d000e65baa80fcc21
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-08 08:29:15 +0000

    et: update 7.0.0 bottle.

--- Formula/e/et.rb
+++ Formula/e/et.rb
@@ -7,13 +7,12 @@ class Et < Formula
   head "https://github.com/MisterTea/EternalTerminal.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/otsge/homebrew-brews2/releases/download/et-6.2.11"
-    sha256 cellar: :any,                 arm64_tahoe:   "7d721c44fdc74dd2359a80fb0ab612f18f54f02f8f4345a745a3b2f2893e8e6e"
-    sha256 cellar: :any,                 arm64_sequoia: "b2a26751a35d228baa5a4fe092dce36a2edeb119d615c396abdadbbb0769829c"
-    sha256 cellar: :any,                 tahoe:         "526f1c7bd88c018d693eb91b618364855f63e982ff8335755f8d04e0b57e3776"
-    sha256 cellar: :any,                 sequoia:       "35bcda0f2340e54e33d42f187a37670ae0d5edde5992a329b56223a60ad8537d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c0e8bb9bcc2e1173b959287408d44ad17fbfd861609a8b31f078d938a84670e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e628e68684a7498462a7eace2baa59da693fd39cb896d66e08551a9fb05c7ff0"
+    root_url "https://github.com/otsge/homebrew-brews2/releases/download/et-7.0.0"
+    sha256 cellar: :any, arm64_tahoe:   "c2ccb2d291a6b2216e31c7848bd5445abfd63dcd4d8ad9bd0384626faa1bf331"
+    sha256 cellar: :any, arm64_sequoia: "e62d6c4424a601bdb96a4b16839f518ac5531513455aac47cf66437513b7b5d8"
+    sha256 cellar: :any, sequoia:       "0d25cf25d830042a486d63185eb674b5a61c7b1cc1d9eeba833dc918a4dcd46d"
+    sha256 cellar: :any, arm64_linux:   "3bed9905999a40bb2b89dde295598f23d314aada885e99c5f3380b1a816c2576"
+    sha256 cellar: :any, x86_64_linux:  "b62f8b44ba432530a07c1a4c3060a33731c74eb81bae8d94481510f0c0a1e8b1"
   end
 
   depends_on "cmake" => :build
