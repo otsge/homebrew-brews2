acf0e94f271c68f665b129a26004975484af6b2c
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-08 09:33:17 +0000

    krokiet: add 12.0.0 bottle.

--- Formula/k/krokiet.rb
+++ Formula/k/krokiet.rb
@@ -6,6 +6,15 @@ class Krokiet < Formula
   license all_of: ["MIT", "CC-BY-4.0"]
   head "https://github.com/qarmin/czkawka.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/otsge/homebrew-brews2/releases/download/krokiet-12.0.0"
+    sha256 cellar: :any, arm64_tahoe:   "b75272b8532aac889a59ca9546951307f7473fa0c007ac12ecc50fcaf6bf2e47"
+    sha256 cellar: :any, arm64_sequoia: "a4d4d37ef96d2ee476be3e0b8bc56072cb769b63e71669e7c7436d238580442a"
+    sha256 cellar: :any, sequoia:       "534f3df005ffbed357c6d31a46c440f0b07f2f26ebf4e702bf51d381635cb23d"
+    sha256 cellar: :any, arm64_linux:   "33e58fa336ba2a3f02a54f7a3592a919e7955c6a072e80bc5d15f0c28f5b40c5"
+    sha256 cellar: :any, x86_64_linux:  "daef47485bc4a0a649d1e807928bc8ac5d190f4a0325521544f9865496d511c5"
+  end
+
   depends_on "rust" => :build
   depends_on "dav1d"
   depends_on "ffmpeg"
